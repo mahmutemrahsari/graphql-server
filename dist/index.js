97b78a4203a8cf712152d21a98a6a309c216b716
Mahmut Emrah Sari <mahmutemrahsari@gmail.com>
2024-01-13 14:22:21 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,64 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
+import { typeDefs } from "./schema.js";
+const resolvers = {
+    //thoose resolvers are entry point to the graph
+    Query: {
+        greeting: () => {
+            return "HELLO HELL!";
+        },
+        tasks() {
+            return db.tasks;
+        },
+        task(_, args) {
+            return db.tasks.find((t) => t.id === args.id);
+        },
+        users() {
+            return db.users;
+        },
+        user(_, args) {
+            return db.users.find((u) => u.id === args.id);
+        },
+    },
+    // Retuns specified user with its tasks
+    User: {
+        tasks(parent) {
+            return db.tasks.filter((t) => t.user_id === parent.id);
+        },
+    },
+    Mutation: {
+        deleteTask(_, args) {
+            db.tasks = db.tasks.filter((task) => task.id !== args.id);
+            return db.tasks;
+        },
+        addTask(_, args) {
+            let task = {
+                ...args.task,
+            };
+            db.tasks.push(task);
+        },
+        updateTask(_, args) {
+            db.tasks = db.tasks.map((t) => {
+                if (t.id === args.id) {
+                    return { ...t, ...args.edits };
+                }
+                return t;
+            });
+            return db.tasks.find((t) => t.id === args.id);
+        },
+    },
+};
+// "_" = parent resolver
+// "args" =  arguments , we can access any query variable sent with the query
+// "context" = context object for supplying context values across all of our resolvers
+// such as authentication information
+// Nested query based on id
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
---
+++ src/_db.ts
@@ -0,0 +1,59 @@
+let users = [
+  {
+    id: "10",
+    name: "Betul",
+  },
+  {
+    id: "11",
+    name: "Hatun",
+  },
+  {
+    id: "12",
+    name: "Mahmut",
+  },
+  {
+    id: "13",
+    name: "Emrah",
+  },
+];
+
+let tasks = [
+  {
+    id: "1",
+    title: "Clean the kitchen",
+    completed: true,
+    user_id: "10",
+  },
+  {
+    id: "2",
+    title: "Make dishes",
+    completed: false,
+    user_id: "12",
+  },
+  {
+    id: "3",
+    title: "Cook Rice",
+    completed: true,
+    user_id: "11",
+  },
+  {
+    id: "4",
+    title: "Clean the toilet",
+    completed: false,
+    user_id: "13",
+  },
+  {
+    id: "5",
+    title: "Check the toilet",
+    completed: false,
+    user_id: "13",
+  },
+  {
+    id: "6",
+    title: "Go to shopping",
+    completed: false,
+    user_id: "11",
+  },
+];
+
+export default { users, tasks };
